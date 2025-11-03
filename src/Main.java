2ecad1263654dfec46a427b851f9fe5d81980349
duyguaydinn <duyguaydinn2001@gmail.com>
2025-11-03 14:51:34 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-        IO.println("i = " + i-1);
-        IO.println("i = " + i-2);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kullanıcı adı: ");
+        String username = sc.nextLine();
+
+        System.out.print("Şifreniz: ");
+        String password = sc.nextLine();
+
+        if (username.equals("admin") && password.equals("123")) {
+            System.out.println("Giriş başarılı!");
+        } else {
+            System.out.println("Hatalı giriş!");
+        }
+
+        sc.close();
     }
 }
